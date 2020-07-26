30744c92488016c70fbafd23c7f36f114a1935b8
HumzaBaig <humzacodemail@gmail.com>
2020-07-26 15:56:49 -0500

    change 0 to 50 in app.js scrolling smooth effect so that each scroll goes to the top of a section underneath the sticky nav

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@
         $('.scrolling-effect a').on('click', function(event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 0
+                scrollTop: $($anchor.attr('href')).offset().top - 50
             }, 1500, 'easeInOutExpo');
             event.preventDefault();
         });
